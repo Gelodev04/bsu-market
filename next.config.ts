52d00827b38c64501f6c36fe5a7198317e100183
gelo <gelodev83@gmail.com>
2025-02-15 22:51:00 +0800

    a

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+},
   images: {
     domains: ["images.remotePatterns", "localhost"],
     remotePatterns: [
