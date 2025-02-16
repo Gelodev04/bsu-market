e34d5c38eaa69f62fd366d44f579edc6cde5bb56
gelo <gelodev83@gmail.com>
2025-02-16 19:33:18 +0800

    removed font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,7 @@
 import type { Metadata } from "next";
 
 import "./globals.css";
-import { Poppins } from "next/font/google";
 
-const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  style: ["normal", "italic"],
-  display: "swap",
-});
 
 export const metadata: Metadata = {
   title: "BSU Market",
@@ -21,8 +14,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="">
-      <body className={`${poppins.className}  antialiased`}>{children}</body>
+    <html lang="en" >
+      <body >{children}</body>
     </html>
   );
 }
