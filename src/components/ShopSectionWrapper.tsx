c8543d4bf605024e27f52c3fecd6baecd0fb50f6
gelo <gelodev83@gmail.com>
2025-02-16 20:12:19 +0800

    updated

--- src/components/ShopSectionWrapper.tsx
+++ src/components/ShopSectionWrapper.tsx
@@ -1,8 +1,23 @@
-
+"use client";
 import { getProducts } from '@/services/api';
 import ShopSection from './ShopSection';
+import { useState, useEffect } from 'react';
 
-export default async function ShopSectionWrapper() {
-  const products = await getProducts();
+export default function ShopSectionWrapper() {
+  const [products, setProducts] = useState([]);
+
+  // Fetch products on the client side
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const data = await getProducts();
+        setProducts(data);
+      } catch (error) {
+        console.error("Failed to fetch products:", error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
   return <ShopSection products={products} />;
 }
